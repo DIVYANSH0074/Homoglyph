1d256ea1bc0bece30e04f6cefb410790f7bbe5e4
DIVYANSH0074 <69522430+DIVYANSH0074@users.noreply.github.com>
2022-09-02 13:49:22 +0530

    Update homoglyph.py

--- homoglyph.py
+++ homoglyph.py
@@ -2,49 +2,62 @@ import re
 import hashlib
 
 
-def str_to_hash(my_string, my_key='data', hash_size=30):
-    h = hashlib.md5(my_key.encode('utf-8'))
-    h.update(my_string.encode('utf-8'))
+def str_to_hash(my_string, my_key="data", hash_size=30):
+    h = hashlib.md5(my_key.encode("utf-8"))
+    h.update(my_string.encode("utf-8"))
     return h.hexdigest()[:hash_size]
 
 
 def homoglyph_regex(pattern):
-    text = pattern.get('regex', [])
+    """
+    Homoglyph executor for ADF Generator
+    :param str pattern: list of regex pattern in single repo adf generator specific
+    :return: list of regex pattern with replaced homoglyph patterns
+    """
+    text = pattern.get("regex", [])
     if text:
         for index, part_pattern in enumerate(text):
-            pattern['regex'][index] = change_dash(part_pattern)
+            pattern["regex"][index] = change_dash(part_pattern)
 
-    split_dict = pattern.get('split_regex', {})
+    split_dict = pattern.get("split_regex", {})
     if split_dict and isinstance(split_dict, dict):
         for split_keys, split_group in split_dict.items():
             if isinstance(split_group, dict):
                 for key, value in split_group.items():
-                    pattern['split_regex'][split_keys][key] = change_dash(value)
+                    pattern["split_regex"][split_keys][key] = change_dash(value)
 
     return pattern
 
 
 def change_dash(part_pattern):
-
-    check_alpha = r'\w-\w'
+    """
+    General function for replacing dash with list of all dash in regex pattern
+    :param str part_pattern: regex containing different types of homoglyph
+    :return: regex replaced with list of homoglyphs
+    """
+    check_alpha = r"\w-\w"
     alpha_pattern = re.finditer(check_alpha, part_pattern)
     alpha_hash = {}
     for obj in alpha_pattern:
         alpha_hash[obj.group()] = str_to_hash(obj.group(), "", 4)
-        part_pattern = part_pattern.replace(obj.group(),str_to_hash(obj.group(), "", 4))
+        part_pattern = part_pattern.replace(
+            obj.group(), str_to_hash(obj.group(), "", 4)
+        )
 
-    check_slash = r'\\[-—–]'
+    check_slash = r"\\[-—–]"
     slash_pattern = re.finditer(check_slash, part_pattern)
     for obj in slash_pattern:
         part_pattern = part_pattern.replace(obj.group(), "-")
 
-    check_brac = r'\\\[|\\]'
+    check_brac = r"\\\[|\\]"
     brac_pattern = re.finditer(check_brac, part_pattern)
     for obj in brac_pattern:
         alpha_hash[obj.group()] = str_to_hash(obj.group(), "", 4)
-        part_pattern = part_pattern.replace(obj.group(), str_to_hash(obj.group(), "", 4))
+        part_pattern = part_pattern.replace(
+            obj.group(), str_to_hash(obj.group(), "", 4)
+        )
 
-    re_dash = r'[-—–]'
+    re_dash = r"[-—–]"
     dash_index = [dash.start() for dash in re.finditer(re_dash, part_pattern)]
 
     if dash_index:
@@ -53,9 +66,9 @@ def change_dash(part_pattern):
             if _dash == 0:
                 split_string.append(part_pattern[0:num_dash])
             else:
-                split_string.append(part_pattern[dash_index[_dash-1]+1:num_dash])
+                split_string.append(part_pattern[dash_index[_dash - 1] + 1 : num_dash])
 
-        split_string.append(part_pattern[dash_index[-1]+1:])
+        split_string.append(part_pattern[dash_index[-1] + 1 :])
 
         for _dash, num_dash in enumerate(dash_index):
             checker = False
@@ -83,15 +96,20 @@ def change_dash(part_pattern):
 
 
 def homoglyph_resolver(uid_regex):
+    """
+    Homoglyph function for resolver
+    :param str uid_regex: uid regex construct from resolver
+    :return: uid regex repalced with list of homoglyphs
+    """
     if uid_regex:
-        check_alpha = r'[a-z]-[a-z]'
+        check_alpha = r"[a-z]-[a-z]"
         alpha_pattern = re.finditer(check_alpha, uid_regex)
         alpha_hash = {}
         for obj in alpha_pattern:
             alpha_hash[obj.group()] = str_to_hash(obj.group(), "", 4)
             uid_regex = uid_regex.replace(obj.group(), str_to_hash(obj.group(), "", 4))
 
-        uid_regex = re.sub(r'[-–—]', '[˗۔‐‑‒–⁃−\\-➖Ⲻ﹘—]', uid_regex)
+        uid_regex = re.sub(r"[-–—]", "[˗۔‐‑‒–⁃−\\-➖Ⲻ﹘—]", uid_regex)
 
         for key, value in alpha_hash.items():
             uid_regex = uid_regex.replace(value, key)
